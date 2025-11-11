fc0d1b895a420cf5d20e5788964abfa87326e9c6
lsb-2817 <h098s02817@chunchon.gwe.hs.kr>
2025-11-11 14:22:32 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     labels[0]: {
        "texts": ["밀은 맛있다"],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
+    #   "videos": ["https://www.youtube.com/watch?v=GGDl66NnOoY"]
     },
 }
 
